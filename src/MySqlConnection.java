bf20276e9a18f18ae517e8112c590500905d7c9c
gustavstarup <gust0001@stud.kea.dk>
2024-03-05 13:17:19 +0100

    MySqlConnection

---
+++ src/MySqlConnection.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySqlConnection {
+    private String database = "jdbc:mysql://localhost:3306/social_medium";
+    private String username = "root";
+    private String password = "YaT1coY8or";
+    private Connection connection = null;
+
+    public MySqlConnection() {
+        createConnection();
+    }
+
+    private void createConnection() {
+        if (connection != null)
+            return; // If connection already created, just return that to ensure singleton
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(database, username, password);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println("EXCEPTION: " + e.getStackTrace());
+        }
+        connection = null;
+    }
+}
